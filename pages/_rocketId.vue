b00493fb230410b7f656664762e7e0e05edcea48
Igor Halfeld <igor@devstacks.com.br>
2019-05-04 17:25:22 -0300

    add more info

--- pages/_rocketId.vue
+++ pages/_rocketId.vue
@@ -1,15 +1,20 @@
 <template>
-  <GridContainer direction="column">
+  <GridContainer direction="column" alignY="center">
     <RocketCard
       fluid
-      :key="`rocket-${index}`"
       :rocket="rocket"
     />
     <GridContainer
-      :styles="{ padding: '5px 30px 30px 30px' }">
+      :styles="{ padding: '5px 30px 30px 30px', maxWidth: '800px' }"
+      direction="column">
       <StyledDescription>
         {{ rocket.description }}
       </StyledDescription>
+      <GridContainer
+        :styles="{ width: '100%', marginTop: '20px' }"
+        alignX="center">
+        <StyledList :items="specs" />
+      </GridContainer>
     </GridContainer>
   </GridContainer>
 </template>
@@ -19,6 +24,7 @@ import StyledTitle from '@/components/Functional/StyledTitle';
 import StyledSubTitle from '@/components/Functional/StyledSubTitle';
 import StyledDescription from '@/components/Functional/StyledDescription';
 import GridContainer from '@/components/Functional/GridContainer';
+import StyledList from '@/components/Functional/StyledList';
 import RocketCard from '@/components/RocketCard';
 
 export default {
@@ -29,10 +35,17 @@ export default {
     StyledDescription,
     GridContainer,
     RocketCard,
+    StyledList,
   },
   async asyncData({ $axios, route }) {
     const rocket = await $axios.$get('/rockets/' + route.params.rocketId);
-    return { rocket };
+    const specs = [
+      { emoji: '🚀' , label: rocket.first_flight },
+      { emoji: '📐', label: `${rocket.height.meters} meters`},
+      { emoji: '🐳', label: `${rocket.mass.kg} kg`},
+    ];
+
+    return { rocket, specs };
   },
 }
 </script>
